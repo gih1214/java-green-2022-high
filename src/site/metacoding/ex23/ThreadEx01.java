070fd3eb0dd72f4a4db36e6928dbad4db9c5cdf9
gih1214 <gih1214@hanmail.net>
2022-02-02 23:56:44 +0900

    4. Thread(스레드) 공부

---
+++ src/site/metacoding/ex23/ThreadEx01.java
@@ -0,0 +1,16 @@
+package site.metacoding.ex23;
+
+public class ThreadEx01 {
+
+    // main 스레드 시작
+    public static void main(String[] args) {
+        System.out.println(1);
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // sleep 단위 : 밀리세컨즈 1/1000초
+
+        System.out.println(2);
+    }
+}
